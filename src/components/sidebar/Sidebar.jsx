215d297573d6db97890bfc68a5f44281df293260
Anshul Sharma <123875259+anshulamrahs@users.noreply.github.com>
2023-04-11 20:51:56 +0530

    sidebar and navbar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,17 @@
 import React from 'react'
 import './sidebar.scss'
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import Person3Icon from '@mui/icons-material/Person3';
+import InventoryIcon from '@mui/icons-material/Inventory';
+import WorkOutlineIcon from '@mui/icons-material/WorkOutline';
+import MopedIcon from '@mui/icons-material/Moped';
+import SsidChartIcon from '@mui/icons-material/SsidChart';
+import CircleNotificationsIcon from '@mui/icons-material/CircleNotifications';
+import SettingsSystemDaydreamIcon from '@mui/icons-material/SettingsSystemDaydream';
+import LogoDevIcon from '@mui/icons-material/LogoDev';
+import SettingsApplicationsIcon from '@mui/icons-material/SettingsApplications';
+import PersonIcon from '@mui/icons-material/Person';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 const Sidebar = () => {
   return (
@@ -7,23 +19,68 @@ const Sidebar = () => {
         <div className="top">
             <span className="logo">Waveyhigh</span>
         </div>
+        <hr/>
         <div className="center">
             <ul>
+                <p className='title'>MAIN</p>
                 <li>
-                    <span className="dashboard">dashboard</span>
+                   <DashboardIcon className="icon"/>
+                   <span className="dashboard">Dashboard</span>
+                </li>
+                <p className='title'>LISTS</p>
+                <li>
+                    <Person3Icon className="icon"/>
+                    <span>Users</span>
                 </li>
                 <li>
-                    <span className="dashboard">dashboard</span>
+                    <InventoryIcon className="icon"/>
+                    <span>Products</span>
                 </li>
                 <li>
-                    <span className="dashboard">dashboard</span>
+                    <WorkOutlineIcon className="icon"/>
+                    <span>Orders</span>
                 </li>
                 <li>
-                    <span className="dashboard">dashboard</span>
+                    <MopedIcon className="icon"/>
+                    <span>Delivery</span>
+                </li>
+                <p className='title'>USEFUL LINKS</p>
+                <li>
+                    <SsidChartIcon className="icon"/>
+                    <span>Stats</span>
+                </li>
+                <li>
+                    <CircleNotificationsIcon className="icon"/>
+                    <span>Notifications</span>
+                </li>
+                <p className='title'>SERVICE</p>
+                <li>
+                    <SettingsSystemDaydreamIcon className="icon"/>
+                    <span>System Health</span>
+                </li>
+                <li>
+                    <LogoDevIcon className="icon"/>
+                    <span>Logs</span>
+                </li>
+                <li>
+                    <SettingsApplicationsIcon className="icon"/>
+                    <span>Settings</span>
+                </li>
+                <p className='title'>USER</p>
+                <li>
+                    <PersonIcon className="icon"/>
+                    <span>Profile</span>
+                </li>
+                <li>
+                    <LogoutIcon className="icon"/>
+                    <span>Logout</span>
                 </li>
             </ul>
         </div>
-        <div className="bottom">color options</div>
+        <div className="bottom">
+            <div className="colorOptions"></div>
+            <div className="colorOptions"></div>
+        </div>
     </div>
   )
 }
